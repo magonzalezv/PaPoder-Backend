f93dd6ad3cb63417fff4ad11b4868e679a2e7c22
unknown <MSI GV62@MSI-GV62-W10PRO.MSI-GV62-W10PRO>
2019-03-01 00:48:57 -0500

    Cors corregido

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 // Inicializar variables
 var app = express();
